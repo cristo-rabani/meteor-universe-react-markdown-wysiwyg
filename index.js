9c9cf9ed2fb5c368499321a715bafef8b77fef13
Mateusz Kilan <mateusz.kilan@vazco.eu>
2016-03-23 16:27:33 +0100

    changing module system from universe to meteor-1.3

---
+++ index.js
@@ -0,0 +1,11 @@
+// export {default as DoubleMarkdown} from './DoubleMarkdownComponent';
+// export {default as ReactMarkdownMediumEditor} from './ReactMarkdownMediumEditor';
+// export {default as ReactHtmlMediumEditor} from './ReactHtmlMediumEditor';
+// export {default as onResize} from './mixins/on-resize';
+// export * from '/editor/rc-tabs';
+
+import default from './DoubleMarkdownComponent.jsx';
+import default from './ReactMarkdownMediumEditor.jsx';
+import default from './ReactHtmlMediumEditor.jsx';
+import default from './mixins/on-resize';
+import '/editor/rc-tabs';
--- package.js
+++ package.js
@@ -11,28 +11,58 @@ Package.describe({
 });
 
 Package.onUse(function (api) {
-    api.versionsFrom('1.2.1');
-    api.use('universe:modules@0.6.4');
-    api.use('vazco:universe-html-purifier@1.2.3');
-    api.use('universe:modules-npm@0.9.7');
-    api.use('universe:utilities-react@0.5.4');
-    api.use('universe:utilities@2.1.0');
-    api.use('underscore@1.0.4');
-    api.use('markdown@1.0.5');
-    api.use('jquery@1.11.4');
-    api.use('universe:i18n@1.2.1');
+    api.versionsFrom('METEOR@1.3-beta.11');
+    // api.use('universe:modules@0.6.4');
+    // DONE api.use('vazco:universe-html-purifier@1.2.3');
+    // api.use('universe:modules-npm@0.9.7');
+    // DONE api.use('universe:utilities-react@0.5.4');
+    // api.use('universe:utilities@2.1.0');
+    // api.use('underscore@1.0.4');
+    // api.use('markdown@1.0.5');
+    // api.use('jquery@1.11.4');
+    // api.use('universe:i18n@1.2.1');
+
+    api.use([
+        'modules@0.5.0-modules.8',
+        'ecmascript@0.4.0-modules.8',
+        'vazco:universe-html-purifier@1.2.3',
+        'universe:utilities-react@1.0.0-beta3',
+        'universe:utilities@2.2.2',
+        'underscore',
+        'markdown@1.0.5',
+        'jquery@1.11.5-modules.8',
+        'universe-i18n@1.3.5'
+    ])
+
+    Npm.depends({
+        "collapse-whitespace": "1.1.2",
+        "to-markdown": "2.0.1",
+        "medium-editor": "5.14.4",
+        "rc-tabs": "5.5.0"
+    });
+
+    api.mainModule('index.js');
 
     api.addFiles([
         'styles/tabs.css',
         'styles/medium-editor.css',
         'styles/medium-editor-theme-beagle.css',
-        'styles/default.css'], 'client');
+        'styles/default.css',
+    ], 'client');
 
-    api.addFiles('en.i18n.yml');
-    api.addFiles('editor.npm.json');
-    api.addFiles('mixins/on-resize.import.jsx');
-    api.addFiles('ReactMarkdownMediumEditor.import.jsx');
-    api.addFiles('ReactHtmlMediumEditor.import.jsx');
-    api.addFiles('DoubleMarkdownComponent.import.jsx');
-    api.addFiles('index.import.js');
+    api.addFiles([
+        'en.i18n.yml',
+        'editor.npm.json',
+        'ReactMarkdownMediumEditor.import.jsx',
+        'ReactHtmlMediumEditor.import.jsx',
+        'DoubleMarkdownComponent.import.jsx'
+    ]);
+
+    // api.addFiles('en.i18n.yml');
+    // api.addFiles('editor.npm.json');
+    // api.addFiles('mixins/on-resize.import.jsx');
+    // api.addFiles('ReactMarkdownMediumEditor.import.jsx');
+    // api.addFiles('ReactHtmlMediumEditor.import.jsx');
+    // api.addFiles('DoubleMarkdownComponent.import.jsx');
+    // api.addFiles('index.import.js');
 });
